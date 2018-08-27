46a3fdaf3067f15e9aa9daa9e829420c84542c1a
Ulasala Sreenath <Ulasala.Sreenath@altisource.com>
2018-08-27 17:03:04 +0530

    committing changes

---
+++ src/collections/MapTest.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+
+    public static void main(String[] args) {
+        Map<String, Object> aMap = new HashMap<>();
+        Long a = 100L;
+        aMap.put("long", a);
+        Long b = (Long) aMap.get("long");
+        System.out.println(b);
+
+    }
+}
